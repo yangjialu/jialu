92e39bf7f35638aabb15a49a57e0f2a29e1fc8f0
yangjialu <573669898@qq.com>
2019-10-05 19:14:28 +0800

    del

--- conftest.py
+++ conftest.py
@@ -1,12 +1,12 @@
 import time
 import pytest
-from selenium.webdriver import Chrome
+from selenium import webdriver
 from pages.login import LoginPage
 
 
 @pytest.fixture(scope="session")
 def init_web():
-    driver = Chrome()
+    driver = webdriver.Remote(command_executor='http://192.168.3.48:4444/wd/hub')
     login_page = LoginPage(driver)
     driver.maximize_window()
     yield driver, login_page
