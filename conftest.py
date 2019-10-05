3913864d71485f20bc4a7694bb96af72408b1cf6
yangjialu <573669898@qq.com>
2019-10-05 19:18:24 +0800

    del

--- conftest.py
+++ conftest.py
@@ -6,7 +6,7 @@ from pages.login import LoginPage
 
 @pytest.fixture(scope="session")
 def init_web():
-    driver = webdriver.Remote(command_executor='http://192.168.3.48:4444/wd/hub')
+    driver = webdriver.Remote(command_executor='http://192.168.3.48:4444/wd/hub', desired_capabilities={'browserName': 'chrome'})
     login_page = LoginPage(driver)
     driver.maximize_window()
     yield driver, login_page
